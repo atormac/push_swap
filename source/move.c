972595c9e9ab68effab0a031ab7afd2d4c908a83
Aarni Torma <atorma@student.hive.fi>
2024-05-28 17:47:18 +0300

    Couple moves implemented

--- source/move.c
+++ source/move.c
@@ -6,19 +6,30 @@
 /*   By: atorma <atorma@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 17:27:16 by atorma            #+#    #+#             */
-/*   Updated: 2024/05/28 17:34:43 by atorma           ###   ########.fr       */
+/*   Updated: 2024/05/28 17:39:56 by atorma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	move_sa(t_stack *a_stack)
+void	move_sa(t_stack *stack)
 {
 	int	tmp;
-	if (a_stack->count_element <= 1)
+	if (stack->count_element <= 1)
 		return ;
-	tmp = a_stack->arr[1];
-	a_stack->arr[1] = a_stack->arr[0];
-	a_stack->arr[0] = tmp;
+	tmp = stack->arr[1];
+	stack->arr[1] = stack->arr[0];
+	stack->arr[0] = tmp;
 
+}
+
+void	move_sb(t_stack *stack)
+{
+	move_sa(stack);
+}
+
+void	move_ss(t_stack *a_stack, t_stack *b_stack)
+{
+	move_sa(a_stack);
+	move_sb(b_stack);
 }
--- source/push_swap.h
+++ source/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: atorma <atorma@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 21:13:48 by atorma            #+#    #+#             */
-/*   Updated: 2024/05/28 17:33:47 by atorma           ###   ########.fr       */
+/*   Updated: 2024/05/28 17:40:06 by atorma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,7 @@ void	array_print(int *arr, int n);
 int		array_is_sorted(int *arr, int n);
 void	stack_print(t_stack *a_stack, t_stack *b_stack, int n);
 void	move_sa(t_stack *a_stack);
+void	move_sb(t_stack *stack);
+void	move_ss(t_stack *a_stack, t_stack *b_stack);
 
 #endif
