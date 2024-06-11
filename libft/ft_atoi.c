eef5d37aec77680abb2a8882ba057015a81bf1c5
Aarni Torma <atorma@student.hive.fi>
2024-06-11 18:57:16 +0300

    atoi fixed

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: atorma <atorma@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 17:34:18 by atorma            #+#    #+#             */
-/*   Updated: 2024/06/10 15:12:25 by atorma           ###   ########.fr       */
+/*   Updated: 2024/06/11 18:56:34 by atorma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,29 @@ static int	overflow_ret(const char *str)
 	return (-1);
 }
 
+static int	is_overflow(const char *str, long res)
+{
+	long	int_min;
+
+	int_min = 2147483648;
+	if (*str == '-')
+	{
+		if (res > (int_min))
+			return (1);
+	}
+	else
+	{
+		if (res > INT_MAX)
+			return (1);
+	}
+	return (0);
+}
+
 int	ft_atoi(const char *str)
 {
-	int			res;
+	long		res;
 	const char	*s;
-	int			overflow;
+	long		prev;
 
 	res = 0;
 	s = str;
@@ -42,13 +60,13 @@ int	ft_atoi(const char *str)
 		s++;
 	while (*s && *s >= '0' && *s <= '9')
 	{
-		overflow = res;
+		prev = res;
 		res = res * 10 + (*s - '0');
-		if (overflow > res)
+		if (is_overflow(str, res))
 			return (overflow_ret(str));
 		s++;
 	}
 	if (res != 0 && *str == '-')
 		res *= -1;
-	return (res);
+	return ((int)(res));
 }
